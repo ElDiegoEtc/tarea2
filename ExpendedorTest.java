f90b73bacd4b2064d6d8224db49eb9a3c2692681
Diego Oyarzo <diegoyarzonavia@gmail.com>
2023-10-20 23:11:43 -0300

    Test clase de expendedor y imagen UML

--- ExpendedorTest.java
+++ ExpendedorTest.java
@@ -1,21 +1,31 @@
 package com.example;
-
+/*
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 
-/*
-public class ExpendedorTest {
-}
-    @Test
-    public void testCuantoVuelto() throws Exception {
-        c = new Comprador(m, 1, e);
 
-        assertThrows(NoHayProductoException.class, () -> {
-            c.cuantoVuelto();
+class ExpendedorTest {
+    private int cantidadProductos;
+    private int eleccion;
+    private Moneda m;
+    @BeforeEach
+    public void setUp(){
+        m = new Moneda1000();
+        eleccion = 2;
+    }
+
+    @Test
+    public void testcomprarProducto() throws Exception {
+        m = null;
+        p = new Producto(Valoresserie(eleccion), );
+
+        assertThrows(PagoIncorrectoException.class, () -> {
+            p.comprarProducto();
         });
     }
-    /*
- */
+
+}*/
